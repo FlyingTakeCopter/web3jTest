6aaf6e970fda065b7ba8bbd41cfcc5d34b779eda
zhaoyu23 <zhaoyu23@jd.com>
2018-09-08 13:56:13 +0800

    添加购买请求

--- app/src/main/java/com/web3jtest/shopping/ProductDetailActivity.java
+++ app/src/main/java/com/web3jtest/shopping/ProductDetailActivity.java
@@ -5,8 +5,9 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.View;
 
+import com.web3jtest.BaseActivity;
 import com.web3jtest.R;
-import com.web3jtest.base.BaseActivity;
+import com.web3jtest.web3.Web3jManager;
 
 /**
  * 万万没想到，啦啦啦啦啦
@@ -24,7 +25,20 @@ public class ProductDetailActivity extends BaseActivity implements View.OnClickL
 
     @Override
     public void onClick(View v) {
-        startActivity(new Intent(this, OrderActivity.class));
+        showLoading("购买中",false);
+        Web3jManager.buy(Web3jManager.getContractList().get(0), Web3jManager.getAccount(0),
+                Web3jManager.getPassword(), 200,new Web3jManager.ReqSellListener() {
+                    @Override
+                    public void onSuccess(String _goodAddr, String _buyer, String _blockAddr) {
+                        startActivity(new Intent(ProductDetailActivity.this, OrderActivity.class));
+                        endLoading();
+                    }
+
+                    @Override
+                    public void onError(Exception _e) {
+                        endLoading();
+                    }
+                });
         overridePendingTransition(0, 0);
     }
 }
